9bffbf24635fa9e6027c691089667a693a925a93
Jesus Mata <matateran@gmail.com>
2022-04-30 22:41:57 -0400

    agregando documento index.php para que heroku me permita subir mi pagina de ejemplo en html

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <title>Bienvenido a Hyperblog 2.0</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+    <div id="container">
+        <div id="cabecera">
+            <img id="logo" src="imagenes/dragon.png" /> Hyperblog
+            <span id="tagline">tu blog de cabecera</span>
+        </div>
+        <div id="post">
+            <h1>Este es el &uacute;ltimo título atractivo del post</h1>
+            <p>y este es el p&aacute;rrafo del inicio donde vamos a explicar las cosas increibles que se pueden hacer con las ramas</p>
+            <p>los blogs son la mejor forma de compartir informaci&oacute;n y tus ideas. Mucha mas que ir a conferencias o salir en youtube. excepto si eres un rockstar. Pero estad&iacute;sticamente no lo eres ......por ahora</p>
+            <p>Suscr&iacute;bete y dale like</p>
+        </div>
+        <div id="footer">
+            Echo con amor Platzi
+        </div>
+    </div>
+</body>
+
+</html>
